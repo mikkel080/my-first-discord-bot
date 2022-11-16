f1ae21ad8f136363b8540dcbac4f2eb57b1122d8
Mikkel Bistrup <mikkel@bistrup.dk>
2022-11-16 11:41:36 +0100

    added mikkel.js

---
+++ commands/mikkel.js
@@ -0,0 +1,16 @@
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+const connection = joinVoiceChannel({
+	channelId: channel.id,
+	guildId: channel.guild.id,
+	adapterCreator: channel.guild.voiceAdapterCreator,
+});
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		return interaction.reply('Pong!');
+	},
+};
